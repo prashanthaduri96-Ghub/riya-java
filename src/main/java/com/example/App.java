129161a9126b722122687f3b06de066888bfbe52
prashanthaduri96-Ghub <prashanthaduri96@gmail.com>
2026-02-15 19:11:37 +0530

    Update App.java in Github

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "Cloud Computing-HYD");
+        model.addAttribute("header", "Prashanth-HYD");
         return "index"; // Return the index.html template
     }
 }
