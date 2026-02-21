939c88a2cad9e9ec678b9adb242885f85082cecb
prashanthaduri96-Ghub <prashanthaduri96@gmail.com>
2026-02-21 06:39:53 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "Prashanth-Hyd-kmm");
+        model.addAttribute("header", "welcome to RIYA ADURI");
         return "index"; // Return the index.html template
     }
 }
